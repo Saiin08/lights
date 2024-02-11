cd039f8d6395dfaff930270c28f21f264f52aea4
Yusaiyin <yusaiyin89@gmail.com>
2024-02-11 15:46:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector(".btn");
+const stopEl = document.querySelector(".stop");
+const walkEl = document.querySelector(".walk");
+
+const wait = () => {
+  setTimeout(() => {
+    stopEl.src = "./img/stop-off.svg";
+    walkEl.src = "./img/walk-on.svg";
+    console.log("Wait for 3 seconds");
+  }, 3000);
+
+  setTimeout(() => {
+    stopEl.src = "./img/stop-on.svg";
+    walkEl.src = "./img/walk-off.svg";
+  }, 8000);
+};
+
+btn.addEventListener("click", wait);
